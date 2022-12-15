4c0f8f7c7f1ca6ec0af32348121738721c8e8c36
Ubuntu <maximeb@dev.i2ce1u0fezoutj3ndzhv5su0dd.fx.internal.cloudapp.net>
2022-12-15 10:38:39 +0000

    feat: add tmux + oh_my_tmux

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # Update stores
-apt update
+sudo apt update
 
 # Install brew as package manager
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/maximeb/.profile
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/maximeb/.profile
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> ~/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install package
-apt install g++ -y       # dependency for neovim plugins
+sudo apt install g++ -y  # dependency for neovim plugins
 brew install node fzf rg # dependency for neovim plugins
 brew install neovim
+brew install tmux
 
 # Configure nvim
 mkdir -p ~/.config/
 cp -r nvim ~/.config/
 
+# Configure tmux
+ls -s -f tmux/.tmux.conf ~/.tmux.conf
+cp tmux/.tmux.conf.local ~/.tmux.conf.local
 
